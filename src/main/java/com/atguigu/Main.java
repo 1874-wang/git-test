24774027e1fc57eb5dcfec65d47ec7b577916e21
1874-wang <1635579857@qq.com>
2024-06-16 22:08:41 +0800

    Update Main.java

--- src/main/java/com/atguigu/Main.java
+++ src/main/java/com/atguigu/Main.java
@@ -3,10 +3,5 @@ package com.atguigu;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
-        System.out.println("hello git");
-        System.out.println("hello lg");
-        System.out.println("hello whg");
-        System.out.println("hello hello hello");
-        System.out.println("hello hello hot-fix");
     }
 }
